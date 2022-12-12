1b163b216899e5662680ce0a3e77b92555cc87d2
MRRazvi <mrrazvi@icloud.com>
2022-12-12 14:12:07 +0500

    working on hub spot command syncing

--- app/Console/Commands/HubSpotCommand.php
+++ app/Console/Commands/HubSpotCommand.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Http\Controllers\HubSpotController;
+use App\Http\Controllers\ServiceBridgeController;
 use App\Models\Estimate;
+use App\Models\ServiceBridgeAccount;
 use Illuminate\Console\Command;
 
 class HubSpotCommand extends Command
@@ -13,11 +16,34 @@ class HubSpotCommand extends Command
 
     public function handle()
     {
-        $hubspot = \HubSpot\Factory::createWithDeveloperApiKey(env('HUBSPOT_API_KEY'));
+        $hs = new HubSpotController(env('HUBSPOT_API_KEY'));
+        $sb_accounts = $this->get_sb_accounts();
 
         $estimates = Estimate::where('synced', false)->get();
         foreach ($estimates as $estimate) {
+            $sb = $sb_accounts[$estimate->sb_account_id];
+            $data = json_decode($estimate->blob);
 
+            $hs_contact = $hs->get_contact($data->Contact->Email);
+            if ($hs_contact) {
+                dump("has contact");
+            } else {
+                dump("no contact");
+            }
         }
+    }
+
+    private function get_sb_accounts()
+    {
+        $data = [];
+
+        $accounts = ServiceBridgeAccount::all();
+        foreach ($accounts as $account) {
+            $sb = new ServiceBridgeController($account->user_id, $account->user_pass);
+            $sb->login();
+            $data[$account->id] = $sb;
+        }
+
+        return $data;
     }
 }
---
+++ app/Http/Controllers/HubSpotController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \HubSpot\Factory;
+use \HubSpot\Client\Crm\Contacts\Model\Filter;
+use \HubSpot\Client\Crm\Contacts\Model\FilterGroup;
+use \HubSpot\Client\Crm\Contacts\Model\SimplePublicObjectInput;
+use \HubSpot\Client\Crm\Contacts\Model\PublicObjectSearchRequest;
+
+class HubSpotController
+{
+    private $client;
+
+    public function __construct($api_key)
+    {
+        $this->client = Factory::createWithDeveloperApiKey($api_key);
+    }
+
+    public function get_contact($email)
+    {
+        $filter = new Filter();
+        $filter->setOperator('EQ')->setPropertyName('email')->setValue($email);
+        $filterGroup = new FilterGroup();
+        $filterGroup->setFilters([$filter]);
+        $searchRequest = new PublicObjectSearchRequest();
+        $searchRequest->setFilterGroups([$filterGroup]);
+        $searchRequest->setProperties([
+            'firstname',
+            'lastname',
+            'email',
+            'phone',
+            'address',
+            'zip',
+            'city',
+            'lifecyclestage',
+            'status_from_sb',
+            'notat_om_aktivitet_i_service_bridge'
+        ]);
+        $contactsPage = $this->client->crm()->contacts()->searchApi()->doSearch($searchRequest);
+
+        if ($contactsPage['total'] < 1) {
+            return false;
+        }
+
+        return $contactsPage['results'][0];
+    }
+
+    public function create_contact($data)
+    {
+        $contactInput = new SimplePublicObjectInput();
+        $contactInput->setProperties($data);
+        $contact = $this->client->crm()->contacts()->basicApi()->create($contactInput);
+
+        dd($contact);
+    }
+}
