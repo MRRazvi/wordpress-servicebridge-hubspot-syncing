3fcdfa5e40b19d074bc44812ea14104c3f6f4dda
MRRazvi <mrrazvi@icloud.com>
2022-12-13 19:32:25 +0500

    logging

--- app/Http/Controllers/HubSpotController.php
+++ app/Http/Controllers/HubSpotController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use \HubSpot\Factory;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use SevenShores\Hubspot\Factory as LegacyFactory;
 use \HubSpot\Client\Crm\Contacts\Model\Filter as ContactFilter;
 use \HubSpot\Client\Crm\Contacts\Model\FilterGroup as ContactFilterGroup;
@@ -29,121 +30,188 @@ class HubSpotController
 
     public function get_contact($email)
     {
-        $filter = new ContactFilter();
-        $filter
-            ->setOperator('EQ')
-            ->setPropertyName('email')
-            ->setValue($email);
-        $filterGroup = new ContactFilterGroup();
-        $filterGroup->setFilters([$filter]);
-        $searchRequest = new ContactPublicObjectSearchRequest();
-        $searchRequest->setFilterGroups([$filterGroup]);
-        $searchRequest->setProperties([
-            'firstname',
-            'lastname',
-            'email',
-            'phone',
-            'address',
-            'zip',
-            'city',
-            'lifecyclestage',
-            'status_from_sb',
-            'notat_om_aktivitet_i_service_bridge'
-        ]);
-        $contactsPage = $this->client->crm()->contacts()->searchApi()->doSearch($searchRequest);
+        try {
+            $filter = new ContactFilter();
+            $filter->setOperator('EQ')->setPropertyName('email')->setValue($email);
+            $filterGroup = new ContactFilterGroup();
+            $filterGroup->setFilters([$filter]);
+            $searchRequest = new ContactPublicObjectSearchRequest();
+            $searchRequest->setFilterGroups([$filterGroup]);
+            $searchRequest->setProperties([
+                'firstname',
+                'lastname',
+                'email',
+                'phone',
+                'address',
+                'zip',
+                'city',
+                'lifecyclestage',
+                'status_from_sb',
+                'notat_om_aktivitet_i_service_bridge'
+            ]);
+            $contactsPage = $this->client->crm()->contacts()->searchApi()->doSearch($searchRequest);
 
-        if ($contactsPage['total'] < 1) {
-            return false;
+            if ($contactsPage['total'] < 1) {
+                return false;
+            }
+
+            return $contactsPage['results'][0];
+        } catch (\Exception $e) {
+            Log::channel('hs-client')->error('get_contact', [
+                'code' => $e->getCode(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'trace' => $e->getTrace()
+            ]);
         }
-
-        return $contactsPage['results'][0];
     }
 
     public function create_contact($data)
     {
-        $contactInput = new ContactInput();
-        $contactInput->setProperties($data);
+        try {
+            $contactInput = new ContactInput();
+            $contactInput->setProperties($data);
 
-        return $this->client->crm()->contacts()->basicApi()->create($contactInput);
+            return $this->client->crm()->contacts()->basicApi()->create($contactInput);
+        } catch(\Exception $e) {
+            Log::channel('hs-client')->error('get_contact', [
+                'code' => $e->getCode(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'trace' => $e->getTrace()
+            ]);
+        }
     }
 
     public function update_contact($contact_id, $data)
     {
-        $data = new ContactSimplePublicObjectInput([
-            'properties' => $data
-        ]);
+        try {
+            $data = new ContactSimplePublicObjectInput([
+                'properties' => $data
+            ]);
 
-        return $this->client->crm()->contacts()->basicApi()->update($contact_id, $data);
+            return $this->client->crm()->contacts()->basicApi()->update($contact_id, $data);
+        } catch(\Exception $e) {
+            Log::channel('hs-client')->error('update_contact', [
+                'code' => $e->getCode(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'trace' => $e->getTrace()
+            ]);
+        }
     }
 
     public function search_deal($contact_id)
     {
-        $hs = LegacyFactory::create($this->api_key);
-        $deals = $hs->deals()->associatedWithContact($contact_id, [
-            'properties' => [
-                'dealname',
-                'amount'
-            ]
-        ]);
+        try {
+            $hs = LegacyFactory::create($this->api_key);
+            $deals = $hs->deals()->associatedWithContact($contact_id, [
+                'properties' => [
+                    'dealname',
+                    'amount'
+                ]
+            ]);
 
-        if ($deals->deals) {
-            return $deals->deals[0];
+            if ($deals->deals) {
+                return $deals->deals[0];
+            }
+
+            return [];
+        } catch(\Exception $e) {
+            Log::channel('hs-client')->error('search_deal', [
+                'code' => $e->getCode(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'trace' => $e->getTrace()
+            ]);
         }
-
-        return [];
     }
 
     public function get_deal($id)
     {
-        return $this->client->crm()->deals()->basicApi()->getById($id);
+        try {
+            return $this->client->crm()->deals()->basicApi()->getById($id);
+        } catch(\Exception $e) {
+            Log::channel('hs-client')->error('get_deal', [
+                'code' => $e->getCode(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'trace' => $e->getTrace()
+            ]);
+        }
     }
 
     public function create_deal($user_id, $data)
     {
-        $response = Http::post(
-            sprintf('%s/deal?hapikey=%s', $this->base_url, $this->api_key),
-            [
-                'associations' => [
-                    'associatedVids' => $user_id
-                ],
-                'properties' => [
-                    [
-                        'name' => 'dealname',
-                        'value' => $data['dealname']
+        try {
+            $response = Http::post(
+                sprintf('%s/deal?hapikey=%s', $this->base_url, $this->api_key),
+                [
+                    'associations' => [
+                        'associatedVids' => $user_id
                     ],
-                    [
-                        'name' => 'amount',
-                        'value' => $data['amount']
-                    ],
-                    [
-                        'name' => 'pipeline',
-                        'value' => $data['pipeline']
-                    ],
-                    [
-                        'name' => 'dealtype',
-                        'value' => $data['dealtype']
-                    ],
-                    [
-                        'name' => 'dealstage',
-                        'value' => $data['dealstage']
-                    ],
-                    [
-                        'name' => 'closedate',
-                        'value' => $data['closedate']
+                    'properties' => [
+                        [
+                            'name' => 'dealname',
+                            'value' => $data['dealname']
+                        ],
+                        [
+                            'name' => 'amount',
+                            'value' => $data['amount']
+                        ],
+                        [
+                            'name' => 'pipeline',
+                            'value' => $data['pipeline']
+                        ],
+                        [
+                            'name' => 'dealtype',
+                            'value' => $data['dealtype']
+                        ],
+                        [
+                            'name' => 'dealstage',
+                            'value' => $data['dealstage']
+                        ],
+                        [
+                            'name' => 'closedate',
+                            'value' => $data['closedate']
+                        ]
                     ]
                 ]
-            ]
-        );
+            );
 
-        return $response->json();
+            return $response->json();
+        } catch(\Exception $e) {
+            Log::channel('hs-client')->error('create_deal', [
+                'code' => $e->getCode(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'trace' => $e->getTrace()
+            ]);
+        }
     }
 
     public function update_deal($deal_id, $data)
     {
-        $data = new DealSimplePublicObjectInput([
-            'properties' => $data
-        ]);
+        try {
+            $data = new DealSimplePublicObjectInput([
+                'properties' => $data
+            ]);
 
-        return $this->client->crm()->deals()->basicApi()->update($deal_id, $data);
+            return $this->client->crm()->deals()->basicApi()->update($deal_id, $data);
+        } catch(\Exception $e) {
+            Log::channel('hs-client')->error('update_deal', [
+                'code' => $e->getCode(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'trace' => $e->getTrace()
+            ]);
+        }
     }
 }
